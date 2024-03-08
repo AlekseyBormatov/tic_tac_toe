f8e883e4275fefc0b8e78302385b97fca833331e
Aleksey Bormatov <inferio3@gmail.com>
2024-03-08 18:30:42 +0300

    add if name

--- game.py
+++ game.py
@@ -1,12 +1,10 @@
-# Объявить класс. game.py
-from gameparts.parts import Board
+from gameparts import Board
 
-# Создать игровое поле - объект класса Board.
-game = Board()
-# Отрисовать поле в терминале.
-game.display()
-# Разместить на поле символ по указанным координатам - сделать ход.
-game.make_move(1, 1, 'X')
-print('Ход сделан!')
-# Перерисовать поле с учётом сделанного хода.
-game.display()
+# Всё, что ниже этой инструкции, не будет импортироваться,
+# но будет выполняться при запуске файла game.py.
+if __name__ == '__main__':
+    game = Board()
+    game.display()
+    game.make_move(1, 1, 'X')
+    print('Ход сделан!')
+    game.display() 
