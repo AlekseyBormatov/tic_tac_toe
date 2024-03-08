55a61910138577328e7d1b3e02b341b8520fae53
Aleksey Bormatov <inferio3@gmail.com>
2024-03-08 20:28:56 +0300

    add imput

--- game.py
+++ game.py
@@ -1,14 +1,15 @@
 from gameparts import Board
 
-# Всё, что ниже этой инструкции, не будет импортироваться,
-# но будет выполняться при запуске файла game.py.
 def main():
     game = Board()
     game.display()
-    game.make_move(1, 1, 'X')
+    # Тут пользователь вводит координаты ячейки.
+    row = int(input('Введите номер строки: '))
+    column = int(input('Введите номер столбца: '))
+    # В метод make_move передаются те координаты, которые ввёл пользователь.
+    game.make_move(row, column, 'X')
     print('Ход сделан!')
     game.display()
 
-# А вот вызов этой функции.
 if __name__ == '__main__':
-    main()
+    main() 
