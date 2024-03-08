e83c517c4da937dbad7e70298e9b08fdc9feba59
Aleksey Bormatov <inferio3@gmail.com>
2024-03-08 18:31:09 +0300

    add main

--- game.py
+++ game.py
@@ -2,9 +2,13 @@ from gameparts import Board
 
 # Всё, что ниже этой инструкции, не будет импортироваться,
 # но будет выполняться при запуске файла game.py.
-if __name__ == '__main__':
+def main():
     game = Board()
     game.display()
     game.make_move(1, 1, 'X')
     print('Ход сделан!')
-    game.display() 
+    game.display()
+
+# А вот вызов этой функции.
+if __name__ == '__main__':
+    main()
